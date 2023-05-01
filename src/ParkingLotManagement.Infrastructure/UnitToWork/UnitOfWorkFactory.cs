8fc957f57853adff9a6b8757cc0324525d99ac6a
Milton Gonzalez Diaz <v-miltong@microsoft.com>
2023-05-01 07:00:01 -0700

    Html form validation

---
+++ src/ParkingLotManagement.Infrastructure/UnitToWork/UnitOfWorkFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotManagement.Infrastructure.UnitToWork
+{
+    public class UnitOfWorkFactory
+    {
+        //public static IUnitOfWork Create()
+        //{
+        //    var connection = new SqlConnection
+        //     (ConfigurationManager.ConnectionStrings("MyDb").ConnectionString);
+        //    connection.Open();
+
+        //    return new AdoNetUnitOfWork(connection, true);
+        //}
+    }
+}
